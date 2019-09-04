e84f6ed7eb3d60faff11106153f60601111ab24a
Katherine Aguilera <katherinam35@gmail.com>
2019-09-04 16:55:29 -0500

    Logout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,26 +11,26 @@ const Home = ({ myList, trends, originals }) => {
     <>
       <Search />
       {myList.length > 0 && (
-      <Categories title='Mi Lista'>
+        <Categories title='Mi Lista'>
           <Carousel>
-            {myList.map(item =>
-              <CarouselItem
-                key={item.id}
-                {...item}
-                isList
-              />
-            )}
-          </Carousel>
-      </Categories>
+          {myList.map((item) => (
+                            <CarouselItem
+  key={item.id}
+  {...item}
+  isList
+/>
+            ))}
+        </Carousel>
+        </Categories>
       )}
       <Categories title='Tendencias'>
         <Carousel>
-          {trends.map((item) => <CarouselItem key={item.id} {...item} />,)}
+          {trends.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
       <Categories title='Originales de Platzi Video'>
         <Carousel>
-          {originals.map((item) => <CarouselItem key={item.id} {...item} />,)}
+          {originals.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
     </>
