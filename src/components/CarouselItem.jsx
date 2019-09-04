959aa3cea2db9d9503669ff3e787f8efe533c270
Katherine Aguilera <katherinam35@gmail.com>
2019-09-04 17:32:34 -0500

    Reproductor URL id

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { setFavorite, deleteFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
@@ -21,29 +22,35 @@ const CarouselItem = (props) => {
     <div className='carousel-item'>
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
-        <div>
-          <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
-          {isList ? (
+          <div>
+          <Link to={`/player/${id}`}>
             <img
               className='carousel-item__details--img'
-              src={removeIcon}
-              alt='Plus Icon'
-              onClick={() => handleDeleteFavorite(id)}
+              src={playIcon}
+              alt='Play Icon'
             />
+          </Link>
+          {isList ? (
+                        <img
+  className='carousel-item__details--img'
+  src={removeIcon}
+  alt='Plus Icon'
+  onClick={() => handleDeleteFavorite(id)}
+/>
           ) : (
-            <img
+              <img
               className='carousel-item__details--img'
-              src={plusIcon}
+  src={plusIcon}
               alt='Plus Icon'
               onClick={handleSetFavorite}
-            />
+/>
           )}
         </div>
-        <p className='carousel-item__details--title'>{title}</p>
-        <p className='carousel-item__details--subtitle'>
+          <p className='carousel-item__details--title'>{title}</p>
+          <p className='carousel-item__details--subtitle'>
           {`${year} ${contentRating} ${duration}`}
         </p>
-      </div>
+        </div>
     </div>
   );
 };
